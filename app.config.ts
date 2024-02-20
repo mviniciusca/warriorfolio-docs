cc545f1edf1d991f1ebb47fd80a5f759588e25f1
Marcos Coelho <mviniciusca@gmail.com>
2024-02-20 18:38:46 -0300

    wip

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
+    title: 'Warriorfolio',
     description: 'The best place to start your documentation.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
@@ -26,13 +26,13 @@ export default defineAppConfig({
     },
     main: {
       padded: true,
-      fluid: true
+      fluid: false
     },
     header: {
       logo: true,
       showLinkIcon: true,
       exclude: [],
-      fluid: true
+      fluid: false
     }
   }
 })
