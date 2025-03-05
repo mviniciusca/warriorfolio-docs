8a5fcc2095397745a2b9061ebdd278948731b48a
Marcos Coelho <mviniciusca@gmail.com>
2025-03-04 22:23:47 -0300

    documentation for v2.1.0

--- app.config.ts
+++ app.config.ts
@@ -1,26 +1,26 @@
 export default defineAppConfig({
-  docus: {
-    title: 'Warriorfolio',
-    description: 'Building the what\'s next',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
-    socials: {
-      twitter: 'marcosvca_',
-      github: 'mviniciusca/warriorfolio',
-    },
-    aside: {
-      level: 0,
-      collapsed: false,
-      exclude: []
-    },
-    main: {
-      padded: true,
-      fluid: false
-    },
-    header: {
-      logo: false,
-      showLinkIcon: true,
-      exclude: [],
-      fluid: false
+    docus: {
+        title: 'Warriorfolio',
+        description: 'Building the what\'s next',
+        image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+        socials: {
+            twitter: 'marcosvca_',
+            github: 'mviniciusca/warriorfolio',
+        },
+        aside: {
+            level: 0,
+            collapsed: false,
+            exclude: []
+        },
+        main: {
+            padded: true,
+            fluid: false
+        },
+        header: {
+            logo: true,
+            showLinkIcon: true,
+            exclude: [],
+            fluid: false
+        }
     }
-  }
 })
